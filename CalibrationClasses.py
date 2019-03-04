4dd00719c9e581bd65a5efc1a9abb598f84eedc3
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2019-03-04 14:04:39 -0500

    p1

--- CalibrationClasses.py
+++ CalibrationClasses.py
@@ -29,62 +29,30 @@ class Calibration:
         """ sample the posterior distribution of the mortality probability """
 
         # specifying the seed of the numpy random number generator
-        np.random.seed(1)
 
         # find values of mortality probability at which the posterior should be evaluated
-        self.mortalitySamples = np.random.uniform(
-            low=CalibSets.POST_L,
-            high=CalibSets.POST_U,
-            size=CalibSets.POST_N)
 
         # create a multi cohort
-        multi_cohort = SurvivalCls.MultiCohort(
-            ids=self.cohortIDs,
-            mortality_probs=self.mortalitySamples,
-            pop_sizes=[CalibSets.SIM_POP_SIZE]*CalibSets.POST_N
-        )
 
         # simulate the multi cohort
-        multi_cohort.simulate(n_time_steps=CalibSets.TIME_STEPS)
 
         # calculate the likelihood of each simulated cohort
-        weights = []
-        for cohort_id in self.cohortIDs:
 
             # get the average survival time for this cohort
-            mean = multi_cohort.multiCohortOutcomes.meanSurvivalTimes[cohort_id]
 
             # construct a gaussian (normal) likelihood
             # with mean calculated from the simulated data and standard deviation from the clinical study.
             # evaluate this pdf (probability density function) at the mean reported in the clinical study.
-            weight = stat.norm.pdf(
-                x=CalibSets.OBS_MEAN,
-                loc=mean,
-                scale=CalibSets.OBS_STDEV)
 
             # store the weight
-            weights.append(weight)
 
         # normalize the likelihood weights
-        sum_weights = np.sum(weights)
-        self.normalizedWeights = np.divide(weights, sum_weights)
 
         # re-sample mortality probability (with replacement) according to likelihood weights
-        self.mortalityResamples = np.random.choice(
-            a=self.mortalitySamples,
-            size=CalibSets.NUM_SIM_COHORTS,
-            replace=True,
-            p=self.normalizedWeights)
 
         # produce the list to report the results
-        for i in range(len(self.mortalitySamples)):
-            self.csvRows.append(
-                [self.cohortIDs[i], self.normalizedWeights[i], self.mortalitySamples[i]])
 
         # write the calibration result into a csv file
-        InOutSupport.write_csv(
-            file_name='CalibrationResults.csv',
-            rows=self.csvRows)
 
     def get_mortality_estimate_credible_interval(self, alpha):
         """
@@ -92,10 +60,8 @@ class Calibration:
         :returns tuple (mean, [lower, upper]) of the posterior distribution"""
 
         # calculate the credible interval
-        sum_stat = StatSupport.SummaryStat(name='Posterior samples',
-                                           data=self.mortalityResamples)
 
-        estimate = sum_stat.get_mean()  # estimated mortality probability
-        credible_interval = sum_stat.get_PI(alpha=alpha)  # credible interval
+        # estimated mortality probability
 
-        return estimate, credible_interval
+        # credible interval
+
